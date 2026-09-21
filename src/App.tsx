b5cbe719666186654126da163b35bc9011582191
Tanvir Hossen Rafi <tanvirhossenar@gmail.com>
2026-09-22 03:16:14 +0800

    Fixed Vercel case sensitivity strictly

--- src/App.tsx
+++ src/App.tsx
@@ -42,7 +42,7 @@ import ProductDetails from './pages/shop/ProductDetails';
 import Cart from './pages/shop/Cart';
 import Wishlist from './pages/shop/Wishlist';
 // @ts-ignore
-import Checkout from './pages/checkout/checkout';
+import Checkout from './pages/shop/Checkout';
 import Categories from './pages/shop/Categories';
 import NewArrivals from './pages/shop/NewArrivals';
 import Contact from './pages/Contact';
